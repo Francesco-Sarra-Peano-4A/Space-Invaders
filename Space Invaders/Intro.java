4aabf2428b17bd17c875967a9f43dd7a061402cc
Francesco-Sarra-Peano-4A <56753334+Francesco-Sarra-Peano-4A@users.noreply.github.com>
2020-01-25 09:49:41 +0100

    STEP 2: Intro Animato

--- Space Invaders/Intro.java
+++ Space Invaders/Intro.java
@@ -15,18 +15,27 @@ import java.awt.Graphics;
  * @author sarra.francesco
  */
 class Intro extends Space {
-
-      @Override
-        public void paint(Graphics g) {
-            g.setColor(Color.blue);
-            g.setFont(new Font("Bold", Font.PLAIN, 40));
-            g.drawString("Space Invaders", 100, 100);
-            g.setFont(new Font("Bold", Font.PLAIN, 10));
-            g.drawString("          by Francesco Sarra", 110, 110);            
+  private int i = 0;
+    
+    void run() throws InterruptedException {
+       setBackground (Color.BLACK);
+       setSize(800, 600);
+               
+        for(i=100;i>=0;i-=1) {
+            System.out.println("i: " + i);
+            this.repaint();
+            Thread.sleep(50);
         }
 
-    void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+      @Override
+        public void paint(Graphics g) {
+             g.setColor(Color.blue);
+        g.setFont(new Font("Bold", Font.PLAIN, 40+i));
+        g.drawString("Space Invaders", 100+i, 100+i);
+        g.setFont(new Font("Bold", Font.PLAIN, 10+i));
+        g.drawString("          by Francesco Sarra", 110+i+i, 110+i+i);           
+        }
 
+ 
 }
